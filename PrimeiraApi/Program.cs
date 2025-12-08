61e7ea7033a710693112a7a83fc8b4d211e9afcc
Paulo Ricardo Stradioti <paulo@paulo.eti.br>
2025-12-08 20:25:23 -0300

    Adicionando Swagger

--- PrimeiraApi/Program.cs
+++ PrimeiraApi/Program.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace PrimeiraApi
 {
@@ -11,8 +15,38 @@ namespace PrimeiraApi
             // 1 - Configurar a Injeção de Dependência: sempre feito no builder.Services
             builder.Services.AddControllers();
             builder.Services.AddDbContext<AppDbContext>();
+            builder.Services.AddSwaggerGen((options) =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "Todo Items API",
+                    Description = "API para gerenciar uma lista de tarefas",
+                    Version = "v1",
+                    Contact = new OpenApiContact()
+                    {
+                        Name = "Paulo Ricardo",
+                        Email = "paulo@paulo.eti.br",
+                        Url = new Uri("https://paulo.bio")
+                    },
+                    License = new OpenApiLicense()
+                    {
+                        Name = "MIT License",
+                        Url = new Uri("https://opensource.org/licenses/MIT")
+                    },
+                    Summary = "Uma API simples para gerenciar tarefas",
+                    TermsOfService = new Uri("https://example.com/terms")
+                });
+
+                var xmlFile = Path.Combine(AppContext.BaseDirectory, $"{Assembly.GetExecutingAssembly().GetName().Name}.xml");
+                options.IncludeXmlComments(xmlFile);
+            });
+
+            //Swagger -> ferramenta pra gerar a documentação da API no formato Open API
 
             var app = builder.Build();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
             // 2 - Aplicar/Usar as Dependências: app
             app.MapControllers();
 
--- PrimeiraApi/TodoController.cs
+++ PrimeiraApi/TodoController.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Net.Mime;
 
 namespace PrimeiraApi
 {
     // P: O que faz a classe ser uma controller? 
     // R: Ela herda da classe ControllerBase
-    [Route("/api/todo")] 
+    [Route("/api/todo")]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [Produces(MediaTypeNames.Application.Json)]
     public class TodoController : ControllerBase
     {
         private readonly AppDbContext appDbContext;
@@ -16,13 +21,26 @@ namespace PrimeiraApi
 
         // P: O que faz um método ser uma action?
         // R: Anotação de Rota / Método(Verbo): []
+        /// <summary>
+        /// Retorna uma lista com todas as tarefas
+        /// </summary>
+        /// <response code="200">Requisição bem Sucedida, retorna a lista de tarefas</response>
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<TodoItem>))]
         public ActionResult GetAll()
         {
             return Ok(appDbContext.Todos);
         }
 
+        /// <summary>
+        /// Retorna os dados detalhados de uma Todo Item em particular.
+        /// </summary>
+        /// <param name="id">É o ID que identifica a Todo Item cujos dados devem ser retornados.</param>
+        /// <response code="404">A tarefa com o ID especificado não foi encontrada.</response>
+        /// <response code="200">A tarefa foi encontrada e seus dados foram retornados com sucesso.</response>
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(TodoItem))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(void))]
         public ActionResult GetById(int id)
         {
             var todo = appDbContext.Todos.Find(id);
@@ -33,16 +51,36 @@ namespace PrimeiraApi
             return Ok(todo);
         }
 
+        /// <summary>
+        /// Cria uma nova tarefa com os dados especificados.
+        /// </summary>
+        /// <param name="item">Dados da Tarefa a serem criados</param>
+        /// <response code="409">Tarefa com o ID especificado já existe.</response>
+        /// <response code="201">Tarefa foi criada com sucesso.</response>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(TodoItem))]
+        [ProducesResponseType(StatusCodes.Status409Conflict, Type = typeof(void))]
         public ActionResult Create([FromBody] TodoItem item)
         {
+            var existing = appDbContext.Todos.Find(item.Id);
+            if (existing != null)
+                return Conflict();
+
             appDbContext.Todos.Add(item);
             appDbContext.SaveChanges();
 
-            return Create(item);
+            return CreatedAtAction(nameof(GetById), new { id = item.Id }, item);
         }
 
+        /// <summary>
+        /// Remove o Todo Item com o Id especificado
+        /// </summary>
+        /// <param name="id">Id da tarefa a ser removida.</param>
+        /// <response code="204">Tarefa removida com sucesso.</response>
+        /// <response code="404">Tarefa não encontrada.</response>
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent, Type = typeof(void))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(void))]
         public ActionResult DeleteById(int id)
         {
             var todo = appDbContext.Todos.Find(id);
@@ -55,8 +93,18 @@ namespace PrimeiraApi
             return NoContent();
         }
 
-
+        /// <summary>
+        /// Atualiza os dados de um Todo Item
+        /// </summary>
+        /// <param name="id">Id do Todo Item a ser atualizado</param>
+        /// <param name="item">Novos dados do Todo Item</param>
+        /// <response code="400">Os ids não correspondem</response>
+        /// <response code="404">A tarefa não foi encontrada</response>
+        /// <response code="204">Atualização feita com sucesso.</response>
         [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent, Type = typeof(void))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(void))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(void))]
         public ActionResult Update(int id, [FromBody] TodoItem item)
         {
             if (id != item.Id)
